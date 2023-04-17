daa2ab9eadb39204358644144d7f3802077c81ff
Dolgov David <dolgov.david.1993@gmail.com>
2023-04-17 19:31:10 +0300

    RadioTest and Radio files modified

--- src/test/java/ru/netology/javaqa/RadioTest.java
+++ src/test/java/ru/netology/javaqa/RadioTest.java
@@ -6,36 +6,46 @@ import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class RadioTest {
 
+    Radio action = new Radio();
+
     @ParameterizedTest
     @CsvFileSource(resources = "/numberRadio.csv")
     public void shouldSetNumberRadio(byte number, byte expected) {
-        Radio numberRadio = new Radio();
 
-        numberRadio.setNumberRadio(number);
-        byte actual = numberRadio.getNumberRadio();
-
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @ParameterizedTest
-    @CsvFileSource(resources = "/controlRadio.csv")
-    public void shouldRemoteControlRadio(byte value,String controlButton, byte expected){
-        Radio action = new Radio();
-
-        action.setNumberRadio(value);
-        action.remoteControlRadio(controlButton);
+        action.setNumberRadio(number);
         byte actual = action.getNumberRadio();
 
         Assertions.assertEquals(expected, actual);
     }
 
     @ParameterizedTest
+    @CsvFileSource(resources = "/controlRadio.csv")
+    public void shouldRemoteControlRadio(byte value, String controlButton, byte expected, byte quantity) {
+        Radio actionWithQuantity = new Radio(quantity);
+
+        actionWithQuantity.setNumberRadio(value);
+        if (controlButton.equals("next")) {
+            actionWithQuantity.next();
+        }
+        if (controlButton.equals("prev")) {
+            actionWithQuantity.prev();
+        }
+        byte actual = actionWithQuantity.getNumberRadio();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
     @CsvFileSource(resources = "/controlVolume.csv")
-    public void shouldRemoteControlVolume(byte value,String controlButton, byte expected){
-        Radio action = new Radio();
+    public void shouldRemoteControlVolume(byte value, String controlButton, byte expected) {
 
         action.setLevelVolume(value);
-        action.remoteControlVolume(controlButton);
+        if (controlButton.equals("+")) {
+            action.nextLevelVolume();
+        }
+        if (controlButton.equals("-")) {
+            action.prevLevelVolume();
+        }
         byte actual = action.getLevelVolume();
 
         Assertions.assertEquals(expected, actual);
